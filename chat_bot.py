3ed5a4727b0af08919d9e3e949d7794af2bdf96f
Sahil Kanungo <kanungosahil123@gmail.com>
2023-03-12 18:39:25 +0530

    updated intents.json

--- chat_bot.py
+++ chat_bot.py
@@ -52,7 +52,7 @@ def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
-        if i['tags'] == tag:
+        if i['tag'] == tag:
             result = random.choice(i['responses'])
             break
 
--- main.py
+++ main.py
@@ -23,9 +23,9 @@ for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
-        documents.append((word_list, intent['tags']))
-        if intent['tags'] not in classes:
-            classes.append(intent['tags'])
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
 
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
